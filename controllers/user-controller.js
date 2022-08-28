3e59fa8285f5678dc1e86732e56b8fbbcae9d6d6
Muhammad <muhamadshehroz10089@icloud.com>
2022-08-28 16:32:28 -0400

    commit

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,7 +4,17 @@ const UserController = {
     getAllUsers(req,res){
         User.find({})
         .populate({
-            path: 'thought','friend'
+            path:'thoughts, friends',
+            select: '-__v'
         })
-    }
+        .select('-__v')
+        .sort({_id:-1})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err =>{
+            console.log(err);
+            res.sendStatus(400);      
+        })
+    },
+
+
 }
